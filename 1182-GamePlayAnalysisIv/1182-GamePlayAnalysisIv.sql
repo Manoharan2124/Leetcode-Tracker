a094339c8fe2628179b4711c97a44e8e949b20e7
Manoharan2124 <manoharan052006@gmail.com>
2026-07-09 15:05:22 +0530

    Create 1182-GamePlayAnalysisIv

---
+++ 1182-GamePlayAnalysisIv/1182-GamePlayAnalysisIv.sql
@@ -0,0 +1,16 @@
+-- Last updated: 7/9/2026, 3:05:21 PM
+SELECT
+    ROUND(
+        COUNT(DISTINCT a1.player_id) /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a1
+JOIN Activity a2
+ON a1.player_id = a2.player_id
+AND DATEDIFF(a2.event_date, a1.event_date) = 1
+WHERE (a1.player_id, a1.event_date) IN (
+    SELECT player_id, MIN(event_date)
+    FROM Activity
+    GROUP BY player_id
+);
