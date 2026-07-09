0a93e067c30716703c379e13549da6054932330f
Manoharan2124 <manoharan052006@gmail.com>
2026-07-09 15:17:09 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/9/2026, 3:17:08 PM
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
