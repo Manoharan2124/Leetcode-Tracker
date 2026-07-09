01477db035dbc984244c5a77c5d585c74bee193f
Manoharan2124 <manoharan052006@gmail.com>
2026-07-09 15:17:55 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 3:17:54 PM
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        return result;
+    }
+
+    private void preorder(TreeNode node, List<Integer> result) {
+
+        if (node == null) {
+            return;
+        }
+
+        result.add(node.val);      // Root
+        preorder(node.left, result);  // Left
+        preorder(node.right, result); // Right
+    }
+}
