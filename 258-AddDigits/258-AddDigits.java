271c8a6e4a1fa0e9fd6940d8c0b4c716752b37c1
Manoharan2124 <manoharan052006@gmail.com>
2026-07-09 15:17:25 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 3:17:24 PM
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
