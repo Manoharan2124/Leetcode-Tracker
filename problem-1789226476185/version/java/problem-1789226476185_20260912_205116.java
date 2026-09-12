47f77fce683c577f00fd4cd9b5045c975157c2bd
Manoharan2124 <manoharan052006@gmail.com>
2026-09-12 20:51:15 +0530

    Create

---
+++ problem-1789226476185/version/java/problem-1789226476185_20260912_205116.java
@@ -0,0 +1,21 @@
+// Last updated: 9/12/2026, 8:51:16 PM
+1class Solution {
+2    public int countSpecialIntegers(int[] nums) {
+3        Map<Integer, List<Integer>> indexMap = new HashMap<>();
+4        for(int i=0;i<nums.length;i++) {
+5            indexMap.computeIfAbsent(nums[i],k -> new ArrayList<>()).add(i);
+6        }
+7        int specialCount = 0;
+8        for(List<Integer> indices : indexMap.values()) {
+9            if(indices.size() == 3) {
+10                int i1 = indices.get(0);
+11                int i2 = indices.get(1);
+12                int i3 = indices.get(2);
+13                if((i2 - i1) == (i3-i2)) {
+14                    specialCount++;
+15                }
+16            }
+17        }
+18        return specialCount;
+19    }
+20} 
